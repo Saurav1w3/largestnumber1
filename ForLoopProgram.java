81a199c9fee18e01c764a70bdb8a8b513942a267
Saurav1w3 <98945876+Saurav1w3@users.noreply.github.com>
2022-10-11 16:30:05 +0530

    Add files via upload

---
+++ ForLoopProgram.java
@@ -0,0 +1,17 @@
+package jdbc;
+
+public class ForLoopProgram {
+
+	public static void main(String[] args) {
+		int i = 0;
+		int j = 0;
+		for(i = 1; i <= 4; i++) {
+			for(j = 1; j <= 4; j++) {
+				System.out.println(i + " "+ j);
+			}
+		}
+		System.out.println(i +" "+ j);
+
+	}
+
+}
