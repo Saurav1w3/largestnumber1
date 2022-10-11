9a9e5a2f30bac2b18cdfa7187341028b06da3e72
Saurav1w3 <98945876+Saurav1w3@users.noreply.github.com>
2022-10-11 16:27:54 +0530

    Add files via upload

---
+++ calcullater.java
@@ -0,0 +1,15 @@
+package jdbc;
+import java.util.*;
+public class calcullater {
+public static void mai(String[]args) {
+	Scanner sc= new Scanner (System.in);
+	int n =sc.nextInt();
+	 int sum =0;
+	for (int i=1;i<=n;i++)
+	{
+		 sum= sum+i;
+		 System.out.println(sum);
+	}
+	}
+}
+
