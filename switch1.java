2d5eecde7e02830827331a9835a4a9f589b95cc4
Saurav1w3 <98945876+Saurav1w3@users.noreply.github.com>
2022-10-11 16:40:23 +0530

    Add files via upload

---
+++ switch1.java
@@ -0,0 +1,18 @@
+package jdbc;
+import java.util.*;
+public class switch1 {
+public static void main (String[]args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("print a number");
+	int button =sc.nextInt();
+	switch (button) {
+	case 1: System.out.println("hello");
+	break;
+	case 2: System.out.println("hello");
+	break;
+	case 3: System.out.println("guruji");
+	break;
+	default: System.out.println("invilid button");
+	}
+}
+}
