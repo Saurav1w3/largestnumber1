ef5e594142b8f1d05c58e145c940e3ba8cc71c7d
Saurav1w3 <98945876+Saurav1w3@users.noreply.github.com>
2022-10-11 16:34:20 +0530

    Add files via upload

---
+++ jfjhdf.java
@@ -0,0 +1,12 @@
+package jdbc;
+public class jfjhdf {
+public static  void main(String[]args) {
+	int i = 0,j = 0;
+	for(int i1=1;i1<4;i1++) {
+		for(int j1=1;j1<4;j1++) {
+			System.out.println(i1+" "+j1);
+		}
+	}
+	System.out.println(i+j);
+}
+}
